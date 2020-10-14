e10e5672733ba64a560591076d55fc289312b068
pawejurczynski <pawel.jurczynski@live.com>
2020-10-14 15:19:46 +0200

    fix(host): add https host

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,2 @@
 const API_KEY = process.env.REACT_APP_OMBD_API_KEY;
-export const OMDB_HOST = `http://omdbapi.com/?apiKey=${API_KEY}`; 
+export const OMDB_HOST = `https://omdbapi.com/?apiKey=${API_KEY}`; 
